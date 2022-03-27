fa171216f373fc5fcb84dc760401bb63ef6ba784
LizaKurochkina <dasha.71.99@mail.ru>
2022-03-27 11:03:41 +0300

    настроила стили у истории изменений

--- src/components/CheckOperations/HistoryChanges.js
+++ src/components/CheckOperations/HistoryChanges.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import classNames from 'classnames'
 import { Button } from '../../views'
 import style from './check_operations.module.scss';
+import table_style from '../CheckTable/check_table.module.scss'
 
 const HistoryChanges = ({
   viewCheck
@@ -12,12 +13,22 @@ const HistoryChanges = ({
     setActiveLine = () => { }
   } = viewCheck
 
+  const setActiveBtn = (e) => {
+    setActiveLine(e.target.name)
+  }
+
   return (
-    <ul>
+    <ul className={style.histoty_btns_container}>
       {historyDatesList.map(line => {
+        const activeStyle = activeLine === line.id ? 'item-btn-active' : ''
         return (
-          <li key={line.id}>
-            <Button>
+          <li key={line.id} className={style.history_item}>
+            <Button
+              variant='text'
+              name={line.id}
+              onClick={setActiveBtn}
+              className={activeStyle}
+            >
               {line.date_time}
             </Button>
           </li>
