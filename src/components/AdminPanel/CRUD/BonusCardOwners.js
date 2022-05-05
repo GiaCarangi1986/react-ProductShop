9a8a8a565931d5cbc486d84a36e9fe2ebdbe1cdd
LizaKurochkina <dasha.71.99@mail.ru>
2022-05-06 00:22:04 +0300

    порефакторила код для crud файлов

--- src/components/AdminPanel/CRUD/BonusCardOwners.js
+++ src/components/AdminPanel/CRUD/BonusCardOwners.js
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import classNames from 'classnames';
-import { Button, Icon, PreloaderPage, ErrorText } from '../../../views';
 import {
   BONUS_CARD_OWNER,
   WIDTH_COL_BONUS_CARD_OWNER,
-  UNITS,
-  MODAL_TYPES,
-  POPUP_TYPES,
 } from '../../../const';
 import { handingErrors } from '../../../utils'
-import style from '../style.module.scss';
-import table_style from '../../CheckTable/check_table.module.scss'
+import ListShow from './ListShow';
 
 import api from '../../../api'
 
@@ -31,21 +25,6 @@ const BonusCardOwners = ({ children, bonus_card }) => {
     }
   }
 
-  // const payOrder = () => {
-  //   api.setWriteOffProducts(bonusCardOwner, currentUser.id)
-  //     .then(res => {
-  //       dispatch('popup/toggle', {
-  //         popup: POPUP_TYPES.admin_panel,
-  //         text: 'Продукты успешно списаны'
-  //       })
-  //       setBonusCardOwner([])
-  //       setError('')
-  //     })
-  //     .catch(err => {
-  //       handleSubmitError(err?.response)
-  //     })
-  // }
-
   useEffect(() => {
     if (!bonusCardOwner.length) {
       setLoading(true)
@@ -63,119 +42,15 @@ const BonusCardOwners = ({ children, bonus_card }) => {
     }
   }, [])
 
-  const classesScroll = classNames({
-    [table_style['table_scroll-horizontal']]: true,
-    [table_style['table_scroll-vertical']]: true,
-  })
-
   return (
-    <div>
-      {children}
-      <div className={classNames(table_style['table-grid'], style.container__right, style.container__right_big)}>
-        <div className={classesScroll}>
-          <div className={table_style['table-layout']}>
-            <table className={table_style.table}>
-              <thead className={table_style['table-head']}>
-                <tr className={table_style['table-row']}>
-                  <th key='edit' className={table_style['table-col']}>
-                    <div style={{ minWidth: '30px', margin: 'auto' }} />
-                  </th>
-                  {Object.keys(BONUS_CARD_OWNER).map(header => {
-                    const w = WIDTH_COL_BONUS_CARD_OWNER[header] || 30
-                    return (
-                      <th key={header} className={table_style['table-col']}>
-                        <div style={{ minWidth: `${w}px`, margin: 'auto' }}>
-                          {BONUS_CARD_OWNER[header]}
-                        </div>
-                      </th>
-                    )
-                  })}
-                  <th key='delete' className={table_style['table-col']}>
-                    <div style={{ minWidth: '30px', margin: 'auto' }} />
-                  </th>
-                </tr>
-              </thead>
-              <tbody className={table_style['table-body']}>
-                {bonusCardOwner.map((line) => {
-                  const classesRow = classNames({
-                    [table_style['table-row']]: true,
-                  })
-                  return (
-                    <tr key={`${line.id}`} className={classesRow}>
-                      <td className={classNames(table_style['table-col'], table_style['table-col-full-rights'])} key='action_colunm'>
-                        <div style={{ minWidth: '25px', margin: 'auto' }} className={style.actions}>
-                          <Button
-                            className='button-edit_action'
-                            title='Редактировать'
-                            name={line.id}
-                            // onClick={handleChange}
-                            variant='text'
-                            data-cy='btn'
-                          >
-                            <Icon slot='icon-left' icon='write' />
-                          </Button>
-                        </div>
-                      </td>
-                      {Object.keys(BONUS_CARD_OWNER).map(product_line => {
-                        const leftOrCenter = Number.isNaN(Number(`${line[product_line]}`));
-                        const tdClasses = classNames({
-                          [table_style['table-col']]: true,
-                          [table_style['table-col_left']]: leftOrCenter
-                        })
-                        const w = WIDTH_COL_BONUS_CARD_OWNER[product_line] || ''
-                        const margin = leftOrCenter ? '' : 'auto'
-                        const value = product_line === BONUS_CARD_OWNER.count ?
-                          line.unit === UNITS[1] ? line[product_line] + ', кг' : line[product_line] + ', шт' :
-                          line[product_line]
-                        return (
-                          <td className={tdClasses} key={product_line}>
-                            <div style={{ minWidth: `${w - 1}px`, margin }}>{value}</div>
-                          </td>
-                        )
-                      })}
-                      <td className={table_style['table-col']} key='action_colunm-delete'>
-                        <div style={{ minWidth: '25px', margin: 'auto' }}>
-                          <Button
-                            className='button-delete_action'
-                            variant='text'
-                            data-cy='btn'
-                            title='Удалить строку'
-                            name={line.id}
-                          // onClick={deleteProduct}
-                          >
-                            <Icon slot='icon-left' icon='deleteIcon' />
-                          </Button>
-                        </div>
-                      </td>
-                    </tr>
-                  )
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div className={style.wrap_row}>
-        <span />
-        <ErrorText errorClass='writeoff'>
-          {error}
-        </ErrorText>
-        <div className={style.wrap_btn}>
-          <Button
-            // onClick={paymentСonfirmation}
-            className='btn_width-100'
-            data-cy='btn'
-            buttonDis
-          >
-            Добавить
-          </Button>
-        </div>
-      </div>
-      {/* {<PreloaderPage loaderClass='admin_panel' />} */}
-      {/* <PayModal
-        headers={{ main: 'Подтвердите списание', text: `Ожидается списание ${productCount} ${productForm}`, btnCancel: 'Отмена', btnOk: 'Списать' }}
-        func={payOrder} /> */}
-    </div>
+    <ListShow
+      children={children}
+      list={bonusCardOwner}
+      WIDTH_COL={WIDTH_COL_BONUS_CARD_OWNER}
+      NAME_COL={BONUS_CARD_OWNER}
+      loading={loading}
+      error={error}
+    />
   )
 }
 
---
+++ src/components/AdminPanel/CRUD/ListShow.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Button, Icon, PreloaderPage, ErrorText } from '../../../views';
+import style from '../style.module.scss';
+import table_style from '../../CheckTable/check_table.module.scss'
+
+const ListShow = ({ children, list = [], WIDTH_COL = {}, NAME_COL = {}, loading = false, error = '' }) => {
+  const classesScroll = classNames({
+    [table_style['table_scroll-horizontal']]: true,
+    [table_style['table_scroll-vertical']]: true,
+  })
+
+  return (
+    <div>
+      {children}
+      <div className={classNames(table_style['table-grid'], style.container__right, style.container__right_big)}>
+        <div className={classesScroll}>
+          <div className={table_style['table-layout']}>
+            <table className={table_style.table}>
+              <thead className={table_style['table-head']}>
+                <tr className={table_style['table-row']}>
+                  <th key='edit' className={table_style['table-col']}>
+                    <div style={{ minWidth: '30px', margin: 'auto' }} />
+                  </th>
+                  {Object.keys(NAME_COL).map(header => {
+                    const w = WIDTH_COL[header] || 30
+                    return (
+                      <th key={header} className={table_style['table-col']}>
+                        <div style={{ minWidth: `${w}px`, margin: 'auto' }}>
+                          {NAME_COL[header]}
+                        </div>
+                      </th>
+                    )
+                  })}
+                  <th key='delete' className={table_style['table-col']}>
+                    <div style={{ minWidth: '30px', margin: 'auto' }} />
+                  </th>
+                </tr>
+              </thead>
+              <tbody className={table_style['table-body']}>
+                {list.map((line) => {
+                  const classesRow = classNames({
+                    [table_style['table-row']]: true,
+                  })
+                  return (
+                    <tr key={`${line.id}`} className={classesRow}>
+                      <td className={classNames(table_style['table-col'], table_style['table-col-full-rights'])} key='action_colunm'>
+                        <div style={{ minWidth: '25px', margin: 'auto' }} className={style.actions}>
+                          <Button
+                            className='button-edit_action'
+                            title='Редактировать'
+                            name={line.id}
+                            variant='text'
+                            data-cy='btn'
+                          >
+                            <Icon slot='icon-left' icon='write' />
+                          </Button>
+                        </div>
+                      </td>
+                      {Object.keys(NAME_COL).map(product_line => {
+                        const leftOrCenter = Number.isNaN(Number(`${line[product_line]}`));
+                        const tdClasses = classNames({
+                          [table_style['table-col']]: true,
+                          [table_style['table-col_left']]: leftOrCenter
+                        })
+                        const w = WIDTH_COL[product_line] || ''
+                        const margin = leftOrCenter ? '' : 'auto'
+                        return (
+                          <td className={tdClasses} key={product_line}>
+                            <div style={{ minWidth: `${w - 1}px`, margin }}>{line[product_line]}</div>
+                          </td>
+                        )
+                      })}
+                      <td className={table_style['table-col']} key='action_colunm-delete'>
+                        <div style={{ minWidth: '25px', margin: 'auto' }}>
+                          <Button
+                            className='button-delete_action'
+                            variant='text'
+                            data-cy='btn'
+                            title='Удалить строку'
+                            name={line.id}
+                          // onClick={deleteProduct}
+                          >
+                            <Icon slot='icon-left' icon='deleteIcon' />
+                          </Button>
+                        </div>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div className={style.wrap_row}>
+        <span />
+        <ErrorText errorClass='writeoff'>
+          {error}
+        </ErrorText>
+        <div className={style.wrap_btn}>
+          <Button
+            // onClick={paymentСonfirmation}
+            className='btn_width-100'
+            data-cy='btn'
+            buttonDis
+          >
+            Добавить
+          </Button>
+        </div>
+      </div>
+      {loading && <PreloaderPage loaderClass='admin_panel' />}
+    </div>
+  )
+}
+
+export default ListShow
