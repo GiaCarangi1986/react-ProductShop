fff4207dd943f771bcd40a72e81ef3dee2648fac
LizaKurochkina <dasha.71.99@mail.ru>
2022-05-06 12:19:16 +0300

    fix date format

--- src/api/serializer/sale.js
+++ src/api/serializer/sale.js
@@ -1,10 +1,12 @@
+import { dateFotmattedForMakeDelivery } from '../../utils/date';
+
 const getSaleSerializer = (params = []) => {
   const serList = []
   params.forEach(el => {
     serList.push({
       id: el.id, // id
-      start_date: el.dateStart, // дата начала проведения акции
-      end_date: el.dateEnd, // дата окончания проведения акции
+      start_date: dateFotmattedForMakeDelivery(el.dateStart), // дата начала проведения акции
+      end_date: dateFotmattedForMakeDelivery(el.dateEnd), // дата окончания проведения акции
       discountPercent: el.discountPercent, // размер скидки
       product_count: el.productCount, // кол-во продуктов, участвующих в акции
     })
