d6e05dbaab13f20887866fcb2a6bc1afcdf80a8f
LizaKurochkina <dasha.71.99@mail.ru>
2022-05-10 14:46:21 +0300

    fix container for history block

--- src/components/CheckOperations/HistoryChanges.js
+++ src/components/CheckOperations/HistoryChanges.js
@@ -25,29 +25,31 @@ const HistoryChanges = ({
 
   return (
     <>
-      <ul className={style.histoty_btns_container}>
-        {addedChecks.map(line => {
-          const activeStyle = activeLine === line.id ? 'item-btn-active' : ''
-          return (
-            <li key={line.id} className={style.history_item}>
-              <Button
-                variant='text'
-                name={{
-                  id: line.id,
-                  linesCheckList: line.linesCheckList,
-                  totalCost: line.totalCost,
-                  cardId: line.cardId,
-                  paid: line.paid
-                }}
-                onClick={setActiveBtn}
-                className={activeStyle}
-              >
-                {`${line.date_time}, ${line.kassirName}`}
-              </Button>
-            </li>
-          )
-        })}
-      </ul>
+      <div className={style.histoty_btns_container_wrap}>
+        <ul className={style.histoty_btns_container}>
+          {addedChecks.map(line => {
+            const activeStyle = activeLine === line.id ? 'item-btn-active' : ''
+            return (
+              <li key={line.id} className={style.history_item}>
+                <Button
+                  variant='text'
+                  name={{
+                    id: line.id,
+                    linesCheckList: line.linesCheckList,
+                    totalCost: line.totalCost,
+                    cardId: line.cardId,
+                    paid: line.paid
+                  }}
+                  onClick={setActiveBtn}
+                  className={activeStyle}
+                >
+                  {`${line.date_time}, ${line.kassirName}`}
+                </Button>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
       <div className={style.block_error}>
         <ErrorText errorClass='form'>
           {error[1]}
